6e8d9d0ed22a2a429d111d0a1a73d179984532f3
glendonw <95762248+glendonw@users.noreply.github.com>
2024-03-27 01:54:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,4 +9,4 @@ sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_genera
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/Redmi/g' package/base-files/files/bin/config_generate
